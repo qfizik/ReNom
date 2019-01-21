1ccda30eb3dc8a2d858791343384d4dba51119ef
suwa <suwa@gridsolar.jp>
2019-01-21 11:12:30 +0900

    Modify doc build shell

--- doc/build.sh
+++ doc/build.sh
@@ -1,10 +1,61 @@
-find rsts/api -type d -name autosum -print0 | xargs -0 rm -rf
-rm -rf _build/html
-if [ ! "$1" == 'NoJP' ]
-  then
-    make -e SPHINXOPTS="-D language='ja'" html
+### Command options
+# default: Build a English document only(same as 'make html'). This is default.
+# -transcript [ja, en]: This generates transcript files(po file) to _locale.
+# -lang [ja, en, all]: This builds documentation for each languages.
+
+# find rsts/api -type d -name autosum -print0 | xargs -0 rm -rf
+CMDNAME=`basename $0`
+
+help() {
+  echo "Usage: $CMDNAME [-t (ja)] [-l (ja, en, all)]"
+  echo ""
+  echo "-t(ja)           This generates transcript files(po files)."
+  echo "-l(en, ja, all)  This builds documentation."
+}
+
+build_as_lang() {
+  rm -rf _build/html
+  if [ "$1" = "ja" ] || [ "$1" = "en" ]; then
+    make -e SPHINXOPTS="-D language=$1" html
+    mv _build/html _build/$1
+  elif [ "$1" = "all" ]; then
+    make -e SPHINXOPTS="-D language=ja" html
     mv _build/html _build/ja
+    make html
+    mv _build/ja _build/html
+  else
+    echo "Usage: $CMDNAME [-l (en, ja, all)]" 1>&2
+    exit 1
+  fi
+}
+
+build_transcript_of() {
+  if [ ! -d _build/gettext ]; then
+    mkdir _build/gettext
+  fi
+
+  if [ "$1" = "ja" ]; then
+    make gettext
+    sphinx-intl update -p _build/gettext -l $1
+  else
+    echo "Usage: $CMDNAME [-t (ja)]" 1>&2
+    exit 1
+  fi
+}
+
+if [ $# -lt 1 ]; then
+  help
+  exit 1
 fi
 
-make html
-mv _build/ja _build/html
+while getopts ":tl-:" OPT
+do
+  case $OPT in
+    "-" ) ;;
+    "l" ) build_as_lang "$2";;
+    "t" ) build_transcript_of "$2";;
+     *  ) help;
+          exit 1 ;;
+  esac
+done
+exit 0
